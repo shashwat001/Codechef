cf67d78acf12aabac6230ed43b38dff394dcd434
shashwat001 <shashwatkmr.001@gmail.com>
2012-12-04 04:13:51 +0530

    finally

--- generator.cpp
+++ generator.cpp
@@ -6,31 +6,16 @@
 int main()
 {
 	srand(time(NULL));
-	//int t = rand()%20 + 1;
-	int t = 200,i,j;
-	int n,m,w,x,y,z = 10000,done[10000];
-	char str[10001];
-	n = 100;
-	printf("%d\n",n);
-	//while(t--)
-	//{
-		
-		for(i = 0;i<n;i++)
-		{
-			memset(done,0,sizeof(done));
-			for(j = 0;j < n;j++)
-			{
-				w = rand()%z;
-				if(done[w]==0)
-				{
-					printf("%d ",w);
-					done[w] = 1;
-				}
-				else
-					j--;
-			}
-			printf("\n");
-		}
-	//}
+	int t = rand()%10 + 1;
+	int n,m,a,b;
+	printf("%d\n",t);
+	while(t--)
+	{
+		n = rand()%400000+2;
+		m = rand()%400000+2;
+		a = rand()%(n-1)+1;
+		b = rand()%(m-1)+1;
+		printf("%d %d %d %d\n",n,m,a,b);
+	}
 	return 0;
 }
